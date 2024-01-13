1903566e5f2023bcd3cbd95af0dd36ca2ebb92aa
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-13 14:01:27 -0700

    Debug forecast endpoint and tests

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,9 +1,8 @@
 class Api::V0::ForecastController < ApplicationController
   def show
     geocoder = GeocodingFacade.new
-    weather_serializer = WeatherSerializer.new
     location = params[:location].split(',')
     lat_lng = geocoder.lat_lon(location[0], location[1])
-    render json: weather_serializer.serialize_weather(lat_lng[:lat], lat_lng[:lon])
+    render json: WeatherSerializer.new.serialize_weather(lat_lng[:lat], lat_lng[:lon])
   end
 end
--- spec/requests/api/v0/forecast_spec.rb
+++ spec/requests/api/v0/forecast_spec.rb
@@ -2,9 +2,55 @@ require 'rails_helper'
 
 RSpec.describe "Forecast" do
   it "get request for /forecast" do
-    forecast = get("/api/v0/forecast?location=denver,co")
+    get '/api/v0/forecast', params: { location: 'latitude,longitude' }
+
     expect(response).to be_successful
 
-    json = JSON.parse(forecast, symbolize_names: true)
+    data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(data).to be_a(Hash)
+    expect(data[:id]).to be nil
+    expect(data[:type]).to eq('forecast')
+
+    attributes = data[:attributes]
+
+    expect(attributes).to be_a(Hash)
+
+    current = attributes[:current_weather]
+
+    expect(current).to be_a(Hash)
+    expect(current[:last_updated]).to be_a(String)
+    expect(current[:temperature]).to be_a(Float)
+    expect(current[:feels_like]).to be_a(Float)
+    expect(current[:humidity]).to be_a(Integer)
+    expect(current[:uvi]).to be_a(Float)
+    expect(current[:visibility]).to be_a(Float)
+    expect(current[:condition]).to be_a(String)
+    expect(current[:icon]).to be_a(String)
+
+    daily = attributes[:daily_weather]
+
+    expect(daily).to be_a(Array)
+    daily.each do |day|
+      expect(day[:date]).to be_a(String)
+      expect(day[:sunrise]).to be_a(String)
+      expect(day[:sunset]).to be_a(String)
+      expect(day[:max_temp]).to be_a(Float)
+      expect(day[:min_temp]).to be_a(Float)
+      expect(day[:condition]).to be_a(String)
+      expect(day[:icon]).to be_a(String)
+    end
+    expect(daily.length).to eq(5)
+
+    hourly = attributes[:hourly_weather]
+
+    expect(hourly).to be_a(Array)
+    hourly.each do |hour|
+      expect(hour[:time]).to be_a(String)
+      expect(hour[:temperature]).to be_a(Float)
+      expect(hour[:conditions]).to be_a(String)
+      expect(hour[:icon]).to be_a(String)
+    end
+    expect(hourly.length).to eq(24)
   end
 end
