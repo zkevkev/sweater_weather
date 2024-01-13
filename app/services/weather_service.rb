274e890d29badca904e361889455c1439da97d6d
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-13 07:31:23 -0700

    Write forecast methods in weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -18,19 +18,46 @@ class WeatherService
         visibility: weather[:vis_miles],
         condition: weather[:condition][:text],
         icon: weather[:condition][:icon]
-      }.to_json
+      }
     end
   end
 
-  def forecast(lat, lon)
-    response = conn.get("forecast.json?key=#{Rails.application.credentials.weatherapi[:key]}&q=#{lat},#{lon}")
+  def forecast_daily(lat, lon)
+    response = conn.get("forecast.json?key=#{Rails.application.credentials.weatherapi[:key]}&q=#{lat},#{lon}&days=5")
     hash = JSON.parse(response.body, symbolize_names: true)
     if hash[:error]
       hash.to_json
     else
-      { 
-        
-      }.to_json
+      weather = hash[:forecast][:forecastday]
+      weather.each do |day|
+        { 
+          date: day[:date],
+          sunrise: day[:astro][:sunrise],
+          sunset: day[:astro][:sunset],
+          max_temp: day[:maxtemp_f],
+          min_temp: day[:mintemp_f],
+          condition: day[:condition][:text],
+          icon: day[:icon]
+        }
+      end    
+    end
+  end
+
+  def forecast_hourly(lat, lon)
+    response = conn.get("forecast.json?key=#{Rails.application.credentials.weatherapi[:key]}&q=#{lat},#{lon}&days=0")
+    hash = JSON.parse(response.body, symbolize_names: true)
+    if hash[:error]
+      hash.to_json
+    else
+      weather = hash[:forecast][:forecastday].first[:hour]
+      weather.each do |hour|
+        { 
+          time: hour[:time].split(' ')[1],
+          temperature: hour[:temp_f],
+          conditions: hour[:condition][:text],
+          icon: hour[:condition][:icon]
+        }
+      end    
     end
   end
 end
