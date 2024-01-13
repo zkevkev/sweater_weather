d4ee0d438e1a8db1d03406a881fc8251009a29a1
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-13 11:38:18 -0700

    Begin debugging forecast endpoint

--- app/controllers/api/v0/forecast_controller.rb
+++ app/controllers/api/v0/forecast_controller.rb
@@ -1,8 +1,9 @@
 class Api::V0::ForecastController < ApplicationController
   def show
-    require 'pry'; binding.pry
     geocoder = GeocodingFacade.new
     weather_serializer = WeatherSerializer.new
-    lat_lng = geocoder.lat_lon(params[:location])
+    location = params[:location].split(',')
+    lat_lng = geocoder.lat_lon(location[0], location[1])
+    render json: weather_serializer.serialize_weather(lat_lng[:lat], lat_lng[:lon])
   end
 end
--- spec/requests/api/v0/forecast_spec.rb
+++ spec/requests/api/v0/forecast_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Forecast" do
   it "get request for /forecast" do
-    get("/api/v0/forecast?location=denver,co")
+    forecast = get("/api/v0/forecast?location=denver,co")
     expect(response).to be_successful
 
-    json = JSON.parse(response.body symbolize_names: true)
+    json = JSON.parse(forecast, symbolize_names: true)
   end
 end
