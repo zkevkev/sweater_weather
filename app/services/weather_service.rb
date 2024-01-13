09d471e9bfa32bc3789841c3c08a5dcf92870c9a
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-13 08:06:39 -0700

    Write tests for weather service

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -6,58 +6,46 @@ class WeatherService
   def current_weather(lat, lon)
     response = conn.get("current.json?key=#{Rails.application.credentials.weatherapi[:key]}&q=#{lat},#{lon}")
     hash = JSON.parse(response.body, symbolize_names: true)
-    if hash[:error]
-      hash.to_json
-    else
-      weather = hash[:current]
-      { 
-        last_updated: weather[:last_updated],
-        feels_like: weather[:feelslike_f],
-        humidity: weather[:humidity],
-        uvi: weather[:uv],
-        visibility: weather[:vis_miles],
-        condition: weather[:condition][:text],
-        icon: weather[:condition][:icon]
-      }
-    end
+    # weather = hash[:current]
+    # { 
+    #   last_updated: weather[:last_updated],
+    #   feels_like: weather[:feelslike_f],
+    #   humidity: weather[:humidity],
+    #   uvi: weather[:uv],
+    #   visibility: weather[:vis_miles],
+    #   condition: weather[:condition][:text],
+    #   icon: weather[:condition][:icon]
+    # }
   end
 
   def forecast_daily(lat, lon)
     response = conn.get("forecast.json?key=#{Rails.application.credentials.weatherapi[:key]}&q=#{lat},#{lon}&days=5")
     hash = JSON.parse(response.body, symbolize_names: true)
-    if hash[:error]
-      hash.to_json
-    else
-      weather = hash[:forecast][:forecastday]
-      weather.each do |day|
-        { 
-          date: day[:date],
-          sunrise: day[:astro][:sunrise],
-          sunset: day[:astro][:sunset],
-          max_temp: day[:maxtemp_f],
-          min_temp: day[:mintemp_f],
-          condition: day[:condition][:text],
-          icon: day[:icon]
-        }
-      end    
-    end
+    # weather = hash[:forecast][:forecastday]
+    # weather.map do |day|
+    #   { 
+    #     date: day[:date],
+    #     sunrise: day[:astro][:sunrise],
+    #     sunset: day[:astro][:sunset],
+    #     max_temp: day[:day][:maxtemp_f],
+    #     min_temp: day[:day][:mintemp_f],
+    #     condition: day[:day][:condition][:text],
+    #     icon: day[:day][:condition][:icon]
+    #   }
+    # end
   end
 
   def forecast_hourly(lat, lon)
     response = conn.get("forecast.json?key=#{Rails.application.credentials.weatherapi[:key]}&q=#{lat},#{lon}&days=0")
     hash = JSON.parse(response.body, symbolize_names: true)
-    if hash[:error]
-      hash.to_json
-    else
-      weather = hash[:forecast][:forecastday].first[:hour]
-      weather.each do |hour|
-        { 
-          time: hour[:time].split(' ')[1],
-          temperature: hour[:temp_f],
-          conditions: hour[:condition][:text],
-          icon: hour[:condition][:icon]
-        }
-      end    
-    end
+    # weather = hash[:forecast][:forecastday].first[:hour]
+    # weather.map do |hour|
+    #   { 
+    #     time: hour[:time].split(' ')[1],
+    #     temperature: hour[:temp_f],
+    #     conditions: hour[:condition][:text],
+    #     icon: hour[:condition][:icon]
+    #   }
+    # end    
   end
 end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -5,14 +5,54 @@ RSpec.describe WeatherService do
     @service = WeatherService.new
   end
 
-  describe '#current_weather' do
-    it 'returns ' do
-      expect(@service.current_weather(48.8567, 2.3508)).to be_a(String)
-      expect(@service.current_weather[:results]).to be_a(Array)
+  describe '#conn' do
+    it 'establishes a connection with weather API' do
+      expect(@service.conn).to be_a(Faraday::Connection)
+    end
+  end
 
-      # results = @service.current_weather[:results]
-      # expect(results[0]).to be_a(Hash)
-      # expect(results[0][:vote_average]).to be_a(Float)
+  describe '#current_weather' do
+    it 'returns a nested hash of weather data' do
+      weather = @service.current_weather(48.8567, 2.3508)
+      current = weather[:current]
+      expect(weather).to be_a(Hash)
+      expect(current).to be_a(Hash)
+      expect(current[:last_updated]).to be_a(String)
+      expect(current[:feelslike_f]).to be_a(Float)
+      expect(current[:humidity]).to be_a(Integer)
+      expect(current[:uv]).to be_a(Float)
+      expect(current[:vis_miles]).to be_a(Float)
+      expect(current[:condition][:text]).to be_a(String)
+      expect(current[:condition][:icon]).to be_a(String)
+    end
+  end
+
+  describe '#forecast_daily' do
+    it 'returns a nested hash of weather data for each day' do
+      weather = @service.forecast_daily(48.8567, 2.3508)
+      daily = weather[:forecast][:forecastday]
+      expect(weather).to be_a(Hash)
+      expect(daily).to be_a(Array)
+      expect(daily[0][:date]).to be_a(String)
+      expect(daily[1][:astro][:sunrise]).to be_a(String)
+      expect(daily[2][:astro][:sunset]).to be_a(String)
+      expect(daily[3][:day][:maxtemp_f]).to be_a(Float)
+      expect(daily[3][:day][:mintemp_f]).to be_a(Float)
+      expect(daily[4][:day][:condition][:text]).to be_a(String)
+      expect(daily[4][:day][:condition][:icon]).to be_a(String)
+    end
+  end
+
+  describe '#forecast_hourly' do
+    it 'returns a nested hash of weather data for each day' do
+      weather = @service.forecast_daily(48.8567, 2.3508)
+      hourly = weather[:forecast][:forecastday].first[:hour]
+      expect(weather).to be_a(Hash)
+      expect(hourly).to be_a(Array)
+      expect(hourly[0][:time]).to be_a(String)
+      expect(hourly[7][:temp_f]).to be_a(Float)
+      expect(hourly[15][:condition][:text]).to be_a(String)
+      expect(hourly[23][:condition][:icon]).to be_a(String)
     end
   end
 end
