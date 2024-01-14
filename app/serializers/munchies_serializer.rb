5532d144c4f70630a4ed3f573041a70f25e4f5ed
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-14 07:44:31 -0700

    Write munchies serializer and tests

--- app/serializers/munchies_serializer.rb
+++ app/serializers/munchies_serializer.rb
@@ -1,5 +1,19 @@
 class MunchiesSerializer
-  def serialize_munchies
-
+  def serialize_munchies(city, state, cuisine)
+    restaurant_facade = RestaurantFacade.new
+    weather_facade = WeatherFacade.new
+    geocoder = GeocodingFacade.new
+    lat_lng = geocoder.lat_lon(city, state)
+    {
+      "data": {
+        "id": nil,
+        "type": "munchie",
+        "attributes": {
+          "destination_city": "#{city}, #{state}",
+          "forecast": weather_facade.current_weather_short(lat_lng[:lat], lat_lng[:lon]),
+          "restaurant": restaurant_facade.restaurant_search(lat_lng[:lat], lat_lng[:lon], cuisine)
+        }
+      }
+    }.to_json
   end
 end
--- spec/serializers/munchies_serializer_spec.rb
+++ spec/serializers/munchies_serializer_spec.rb
@@ -8,7 +8,34 @@ RSpec.describe MunchiesSerializer do
   describe '#serialize_munchies' do
     it 'returns a nested hash of weather and restaurant data for a location' do
       response = @serializer.serialize_munchies(39.74001, -104.99202, 'italian')
+      munchies = JSON.parse(response, symbolize_names: true)
+
+      expect(munchies).to be_a(Hash)
+
+      data = munchies[:data]
+
+      expect(data).to be_a(Hash)
+      expect(data[:id]).to be nil
+      expect(data[:type]).to eq('munchie')
+
+      attributes = data[:attributes]
+
+      expect(attributes).to be_a(Hash)
+      expect(attributes[:destination_city]).to be_a(String)
+
+      forecast = attributes[:forecast]
       
+      expect(forecast).to be_a(Hash)
+      expect(forecast[:summary]).to be_a(String)
+      expect(forecast[:temperature]).to be_a(Float)
+
+      restaurant = attributes[:restaurant]
+
+      expect(restaurant).to be_a(Hash)
+      expect(restaurant[:name]).to be_a(String)
+      expect(restaurant[:address]).to be_a(String)
+      expect(restaurant[:rating]).to be_a(Float)
+      expect(restaurant[:reviews]).to be_a(Integer)
     end
   end
 end
