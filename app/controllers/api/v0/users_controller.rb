7ed2fad552a1fb7ca3796c41c2f18b63c13814d8
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-13 18:38:20 -0700

    Outline logic for users create endpoint

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V0::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    if user.save!
+      render json: UserSerializer.new # method
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation) # :api_key
   end
 end
