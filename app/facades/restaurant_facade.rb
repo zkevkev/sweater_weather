ee86e68310a93cc37094d5daf5bfb4209fd52748
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-14 09:04:51 -0700

    Refactor restaurant facade to use restaurant poro

--- app/facades/restaurant_facade.rb
+++ app/facades/restaurant_facade.rb
@@ -1,12 +1,13 @@
 class RestaurantFacade
   def restaurant_search(lat, lon, cuisine)
     service = RestaurantService.new
-    restaurant = service.search(lat, lon, cuisine)[:businesses].first
+    response = service.search(lat, lon, cuisine)[:businesses].first
+    restaurant = Restaurant.new(response)
     {
-      name: restaurant[:name],
-      address: restaurant[:location][:display_address].first,
-      rating: restaurant[:rating],
-      reviews: restaurant[:review_count]
+      name: restaurant.name,
+      address: restaurant.address,
+      rating: restaurant.rating,
+      reviews: restaurant.reviews
     }
   end
 end
