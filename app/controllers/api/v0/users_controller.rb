519cf89b3e7f921e8d40b7af8dc4463715c8fcbf
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-15 11:43:02 -0700

    Debug create user endpoint (tests bugged, postman functional)

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,11 +1,11 @@
 class Api::V0::UsersController < ApplicationController
   def create
-    json_data = JSON.parse(request.body, symbolize_names: true)
-    user = User.new(user_params(json_data))
+    data = JSON.parse(request.body.read)
+    user = User.new(user_params(data))
     user.api_key = SecureRandom.hex(16)
 
     if user.save
-      render json: UserSerializer.new(user).serialized_json
+      render json: UserSerializer.new(user)
     else
       render json: { errors: user.errors }, status: :unprocessable_entity
     end
@@ -14,6 +14,12 @@ class Api::V0::UsersController < ApplicationController
   private
 
   def user_params(data)
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    # params.require(:user).permit(:email, :password, :password_confirmation)
+
+    user_params = {
+      email: data['email'],
+      password: data['password'],
+      password_confirmation: data['password_confirmation']
+    }
   end
 end
--- spec/requests/api/v0/create_user_spec.rb
+++ spec/requests/api/v0/create_user_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'get request for /user' do
   context 'with valid parameters' do
-    it 'creates a new user' do
+    it 'creates a new user and returns their api key' do
       user_info = {
         email: 'example@example.com',
-        password: 'password123'
+        password: 'password123',
+        password_confirmation: 'password123'
       }.to_json
 
-      post '/api/v0/user', params: user_info, headers: { 'Content-Type' => 'application/json' }
+      post '/api/v0/user', params: user_info, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
 
       expect(response).to be_successful
       expect(response.status).to eq(201)
@@ -28,6 +29,22 @@ RSpec.describe 'get request for /user' do
       expect(attributes).to be_a(Hash)
       expect(attributes[:email]).to be_a(String)
       expect(attributes[:api_key]).to be_a(String)
+    end
+  end
+
+  context 'with invalid parameters' do
+    it 'returns an appropriate error' do
+      user_info = {
+        email: 'example@example.com',
+        password: 'password123',
+        password_confirmation: 'password'
+      }.to_json
+
+      post '/api/v0/user', params: user_info, headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      expect(response.errors).to eq("doesn't match Password")
     end
   end
 end
