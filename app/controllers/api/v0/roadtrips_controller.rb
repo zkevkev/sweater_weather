68a006a48d19eccee873f6423000682d86d08459
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-15 15:38:22 -0700

    Outline roadtrip create action logic

--- app/controllers/api/v0/roadtrips_controller.rb
+++ app/controllers/api/v0/roadtrips_controller.rb
@@ -1,5 +1,24 @@
 class Api::V0::RoadtripsController < ApplicationController
   def create
+    data = JSON.parse(request.body.read)
+    roadtrip_data = roadtrip_params(data)
+    user = User.find_by(api_key: roadtrip_data[:api_key])
 
+    if user
+      render json: RoadtripSerializer.new.serialize_roadtrip(roadtrip_data[:origin], roadtrip_data[:destination])
+    # elsif a field is left blank (move above)
+    else
+      render json: { errors: 'Invalid api key, please try again' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def roadtrip_params(data)
+    roadtrip_params = {
+      origin: data['origin'],
+      destination: data['destination'],
+      api_key: data['api_key']
+    }
   end
 end
