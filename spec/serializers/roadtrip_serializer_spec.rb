239d47bc12f1addb1a49761cee4e86393e9add2f
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-16 10:01:01 -0700

    Write tests for roadtrip serializer

--- spec/serializers/roadtrip_serializer_spec.rb
+++ spec/serializers/roadtrip_serializer_spec.rb
@@ -8,6 +8,28 @@ RSpec.describe RoadtripSerializer do
   describe '#serialize_roadtrip' do
     it 'returns a nested hash of roadtrip data' do
       response = @serializer.serialize_roadtrip('Cincinatti,OH', 'Chicago,IL')
+
+      expect(response).to be_a(Hash)
+
+      data = response[:data]
+
+      expect(data).to be_a(Hash)
+      expect(data[:id]).to be nil
+      expect(data[:type]).to eq('road_trip')
+
+      attributes = data[:attributes]
+
+      expect(attributes).to be_a(Hash)
+      expect(attributes[:start_city]).to be_a(String)
+      expect(attributes[:end_city]).to be_a(String)
+      expect(attributes[:travel_time]).to be_a(String)
+
+      weather_at_eta = attributes[:weather_at_eta]
+
+      expect(weather_at_eta).to be_a(Hash)
+      expect(weather_at_eta[:datetime]).to be_a(String)
+      expect(weather_at_eta[:temperature]).to be_a(Float)
+      expect(weather_at_eta[:condition]).to be_a(String)
     end
   end
 end
